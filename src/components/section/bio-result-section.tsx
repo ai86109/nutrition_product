848d6e4a0c345d33c1dec572539dc7859f5b6825
derek <ai86109@gmail.com>
2025-10-16 17:09:09 +0800

    refactor: use tailwind to handle container style

--- src/components/section/bio-result-section.tsx
+++ src/components/section/bio-result-section.tsx
@@ -8,13 +8,13 @@ import ProteinCard from "../bio-result/protein-card"
 
 export default function BioResultSection() {
   return (
-    <CardContent className="flex-1 bio-cards-container">
-      <div className="bio-cards-grid">
-        <BmiCard />
-        <CalorieCard />
-        <TdeeCard />
-        <ProteinCard />
-      </div>
+    <CardContent className="flex-1 @container">
+        <div className="grid grid-cols-1 gap-4 @[40rem]:grid-cols-2 @[64rem]:grid-cols-4">
+          <BmiCard />
+          <CalorieCard />
+          <TdeeCard />
+          <ProteinCard />
+        </div>
     </CardContent>
   )
 }
