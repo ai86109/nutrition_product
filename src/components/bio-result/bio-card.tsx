66554044d9c018a3a2db74cafd0bdac76a66dbb9
derek <ai86109@gmail.com>
2025-11-12 14:22:39 +0800

    feat: add abw

---
+++ src/components/bio-result/bio-card.tsx
@@ -0,0 +1,34 @@
+import { Card, CardContent } from "@/components/ui/card"
+import InfoPopover from "../info-popover"
+import ConditionalContent from "./conditional-content"
+import { useBioInfoCalculations } from "@/hooks/useBioInfoCalculations"
+
+export default function BioCard() {
+  const { bmi, abw } = useBioInfoCalculations()
+  const isValidBMI = bmi > 0
+  
+  return (
+    <Card className="max-h-[120px]">
+      <CardContent>
+        <div className="flex items-center gap-1">
+          <ConditionalContent condition={isValidBMI} fallback="請填寫身高、體重來計算 BMI">
+            <span className="font-bold">BMI：</span>
+            <span>{bmi}</span>
+          </ConditionalContent>
+          <InfoPopover>
+            <p>BMI = 體重（公斤）/ 身高²（公尺）</p>
+          </InfoPopover>
+        </div>
+        <div className="flex items-center gap-1 mt-4">
+          <ConditionalContent condition={isValidBMI} fallback="請填寫身高、體重來計算調整體重">
+            <span className="font-bold">調整體重：</span>
+            <span>{abw}</span>
+          </ConditionalContent>
+          <InfoPopover>
+            <p>調整體重 = 理想體重+ 0.25 * (實際體重 - 理想體重)</p>
+          </InfoPopover>
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
--- src/components/section/bio-result-section.tsx
+++ src/components/section/bio-result-section.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { CardContent } from "@/components/ui/card"
-import BmiCard from "../bio-result/bmi-card"
+import BioCard from "../bio-result/bio-card"
 import CalorieCard from "../bio-result/calorie-card"
 import TdeeCard from "../bio-result/tdee-card"
 import ProteinCard from "../bio-result/protein-card"
@@ -10,7 +10,7 @@ export default function BioResultSection() {
   return (
     <CardContent className="flex-1 @container">
         <div className="grid grid-cols-1 gap-4 @[40rem]:grid-cols-2 @[64rem]:grid-cols-4">
-          <BmiCard />
+          <BioCard />
           <CalorieCard />
           <TdeeCard />
           <ProteinCard />
